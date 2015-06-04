dadde31b1ea20c03baca8301b8d35935f78892cf
Peter-White <pwhite007@icloud.com>
2015-06-04 09:59:52 -0400

    Ajax Successful

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ $(function (){
     url: 'http://localhost:3000/signers',
     success: function(signers) {
       $.each(signers, function(i, signer) {
-        $signers.append('<li>first name: '+ signer.first_name +', last name: '+ signer.last_name +'</li>');
+        $signers.append('<li>'+ signer.first_name +' '+ signer.last_name +'</li>');
     });
   },
   error: function() {
@@ -21,30 +21,29 @@ $(function (){
   }
 });
 
-  $('#submit').on('click', function() {
+$('#submit').on('click', function() {
 
-    var signer = {
-      first_name: $first_name.val(),
-      last_name: $last_name.val(),
-      age: $age.val(),
-      address: $address.val(),
-      email: $email.val(),
-      quote: $quote.val(),
-    };
+  var signer = {signer:{
+    first_name: $first_name.val(),
+    last_name: $last_name.val(),
+    age: $age.val(),
+    address: $address.val(),
+    email: $email.val(),
+    quote: $quote.val(),
+  }};
 
-    $.ajax({
-      type: 'POST',
-      url: 'http://localhost:3000/signers',
-      data: signer,
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(newSigner) {
-        $signers.append('<li>first name: '+ newSigner.first_name +', last name: '+ newSigner.last_name +'</li>');
-      },
-      error: function() {
-        alert('error saving signers');
-      }
-      });
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/signers',
+    data: signer,
+    success: function(newSigner) {
+      $signers.append('<li>first name: '+ newSigner.first_name +', last name: '+ newSigner.last_name +'</li>');
+      alert('Thank you for caring');
+    },
+    error: function() {
+      alert('error saving signers');
+    }
     });
-
   });
+
+});
