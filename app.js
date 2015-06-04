934c0f57a39d238f6ab5314badc0213e6c45a59b
Peter-White <pwhite007@icloud.com>
2015-06-04 12:27:25 -0400

    Added new div boxes

--- app.js
+++ app.js
@@ -8,21 +8,20 @@ $(function (){
   var $email = $('#email');
   var $quote = $('#quote');
 
-  $.ajax({
-    type: 'GET',
-    url: 'http://localhost:3000/signers',
-    success: function(signers) {
-      $.each(signers, function(i, signer) {
-        $signers.append('<li>'+ signer.first_name +' '+ signer.last_name +'</li>');
-    });
-  },
+$.ajax({
+  type: 'GET',
+  url: 'http://localhost:3000/signers',
+  success: function(signers) {
+    $.each(signers, function(i, signer) {
+      $signers.append('<li>'+ signer.first_name +' '+ signer.last_name +'</li>');
+  });
+},
   error: function() {
     alert('error loading signers');
   }
 });
 
 $('#submit').on('click', function() {
-
   var signer = {signer:{
     first_name: $first_name.val(),
     last_name: $last_name.val(),
@@ -37,13 +36,16 @@ $('#submit').on('click', function() {
     url: 'http://localhost:3000/signers',
     data: signer,
     success: function(newSigner) {
-      $signers.append('<li>first name: '+ newSigner.first_name +', last name: '+ newSigner.last_name +'</li>');
+      $signers.append('<li>'+ newSigner.first_name +' '+ newSigner.last_name +'</li>');
       alert('Thank you for caring');
     },
     error: function() {
-      alert('error saving signers');
+      alert('I\'m sorry but your submission information is invalid');
     }
     });
   });
+});
 
+$('#counter').show(function (){
+var $count = $("#contributions ul").length;
 });
