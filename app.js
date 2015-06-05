c5b5c2b64ecadeee04d6c77a6fe893253d329daf
Peter-White <pwhite007@icloud.com>
2015-06-05 14:30:34 -0400

    Everything Works

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ $(function (){
   var $email = $('#email');
   var $quote = $('#quote');
   var $count = $('#counter');
+  var $print_quote = $('#printquote');
+
+var reset = function() {
+  $('#signers').val('');
+  $('#first_name').val('');
+  $('#last_name').val('');
+  $('#age').val('');
+  $('#address').val('');
+  $('#email').val('');
+  $('#quote').val('');
+  $('#counter').val('');
+  $('#printquote').val('');
+};
+
 
 $.ajax({
   type: 'GET',
@@ -15,7 +29,7 @@ $.ajax({
   success: function(signers) {
     $.each(signers, function(i, signer) {
       // $signers.append('<li>'+ signer.first_name +' '+ signer.last_name +'</li>');
-      $signers.append('<li><a href="http://localhost:3000/signers/' + signer.id + '">' + signer.first_name + ' ' + signer.last_name + '</a></li>');
+      $signers.append('<li data-id="' + signer.id + '">' + signer.first_name + ' ' + signer.last_name + '</li>');
   });
 },
   error: function() {
@@ -38,8 +52,10 @@ $('#submit').on('click', function() {
     url: 'http://localhost:3000/signers',
     data: signer,
     success: function(newSigner) {
-      $signers.append('<li>'+ newSigner.first_name +' '+ newSigner.last_name +'</li>');
+      $signers.append('<li data-id="' + newSigner.id + '">' + newSigner.first_name +' '+ newSigner.last_name +'</li>');
       alert('Thank you for caring');
+      reset();
+      getCount();
     },
     error: function() {
       alert('I\'m sorry but your submission information is invalid');
@@ -47,18 +63,28 @@ $('#submit').on('click', function() {
     });
   });
 
-  $('#counter').show(function (){
-
-    $.ajax({
+var getCount = function(){
+      $.ajax({
     type: 'GET',
     url: 'http://localhost:3000/signers-count',
     success: function(count) {
       $count.text(count.count);
-    }
+     }
+    });
+}
+$('#counter').show(function (){
+  getCount();
+});
+
+
+
+  $("#signers").on('click', 'li', function(event){
+    var id = $(this).data('id');
+    $.ajax({
+      url: 'http://localhost:3000/signers/' + id,
+      type: 'GET',
+    }).done(function(response){
+      $("#printquote").text(response.quote)
     });
   });
-
-// $('#signers li a').on('click', function(e) {
-// e.preventDefault();
-//   });
 });
